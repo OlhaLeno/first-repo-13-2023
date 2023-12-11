9f0aecc08cde8596331e5a6faa6b0229cfd93145
Olha <151624725+OlhaLeno@users.noreply.github.com>
2023-12-11 13:02:39 +0200

    Update лаба.5.py

--- лаба.5.py
+++ лаба.5.py
@@ -1,28 +1,28 @@
 class Fish:
-    def __init__(self, name, age, species, size, prefferedFood, isAggressive, neededSpace):
+    def __init__(self, name, age, species, size, preffered_food, is_aggressive, needed_space):
         self.name = name
         self.age = age
         self.species = species
         self.size = size
-        self.prefferedFood = prefferedFood
-        self.isAggressive = isAggressive
-        self.neededSpace = neededSpace
+        self.preffered_food = preffered_food
+        self.is_aggressive = is_aggressive
+        self.needed_space = needed_space
 
 
 class Aquarium:
-    def __init__(self, totalVolume):
-        self.totalVolume = totalVolume
-        self.freeSpace = totalVolume
+    def __init__(self, total_volume):
+        self.total_volume = total_volume
+        self.free_space = total_volume
         self.fish_list = []
 
     def add_fish(self, fish):
-        if fish.isAggressive and self.freeSpace >= fish.neededSpace:
+        if fish.is_aggressive and self.free_space >= fish.needed_space:
             self.fish_list.append(fish)
-            self.freeSpace -= fish.neededSpace
+            self.free_space -= fish.needed_space
             print(f"Рибка {fish.name} додана до акваріуму.")
-        elif not fish.isAggressive:
+        elif not fish.is_aggressive:
             self.fish_list.append(fish)
-            self.freeSpace -= fish.neededSpace
+            self.free_space -= fish.needed_space
             print(f"Рибка {fish.name} додана до акваріуму.")
         else:
             print(f"Не можна додати рибку {fish.name} через агресивність або нестачу місця.")
@@ -35,18 +35,24 @@ class Aquarium:
 
 
 if __name__ == "__main__":
-    aquarium1 = Aquarium(totalVolume=10)
-    aquarium2 = Aquarium(totalVolume=15)
+    aquarium1 = Aquarium(total_volume=35)
+    aquarium2 = Aquarium(total_volume=20)
 
     fish1 = Fish("Дорі", 2, "Блакитний хірург", 5, "Водорості", False, 3)
-    fish2 = Fish("Норі", 4, "Акула", 12, "Дрібна риба", True, 9)
+    fish2 = Fish("Норі", 4, "Акула", 12, "Дрібна риба", True, 10)
     fish3 = Fish("Немо", 1, "Клоун", 3, "Планктон", False, 1)
     fish4 = Fish("Локі", 3, "Піранья", 7, "М'ясо", True, 6)
-
-    aquarium1.add_fish(fish1)
+    fish5 = Fish("Сенді", 2, "Золота рибка", 2, "Корм", False, 4)
+    fish6 = Fish("Корді", 5, "Блакитний тан", 7, "Водорості", False, 5)
+    fish7 = Fish("Ідол", 3, "Мавританський ідол", 10, "Дрібна риба", True, 9)
+    
+    aquarium1.add_fish(fish2)
     aquarium1.add_fish(fish4)
+    aquarium1.add_fish(fish7)
+    aquarium2.add_fish(fish1)
     aquarium2.add_fish(fish3)
-    aquarium2.add_fish(fish2)
+    aquarium2.add_fish(fish5)
+    aquarium2.add_fish(fish6)
 
     aquarium1.get_3_largest_fishes()
     aquarium2.get_3_largest_fishes()
